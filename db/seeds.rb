61dd0e0013079ef4288ff5ca2b2d04bc2759a7c4
sbaxter95 <sbaxter95@outlook.com>
2017-10-03 23:58:51 +0100

    run the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+c1 = Cat.create!(name: 'Leo', breed: 'British Shorthair')
+c2 = Cat.create!(name: 'Orlando', breed: 'Bombay')
+c3 = Cat.create!(name: 'Cavendish', breed: 'Siamese')
+
+h1 = House.create!(number: 39, street: 'High Street')
+h2 = House.create!(number: 54, street: 'Old Street')
+
+CatHouse.create!(house_id: h1.id, cat_id: c1.id)
+CatHouse.create!(house_id: h2.id, cat_id: c2.id)
+CatHouse.create!(house_id: h1.id, cat_id: c3.id)
